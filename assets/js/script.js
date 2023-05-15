42b567cf4a7fffbe85001bba369d5edd342083eb
leec313 <>
2023-05-15 17:05:36 +0000

    added code to increment correct and incorrect display

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,8 +35,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -58,10 +60,15 @@ function calculateCorrectAnswer() {
 
 function incrementScore() {
 
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
